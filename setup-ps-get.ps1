39786e691cfc1128711aadb8b99552a4780ea106
Kyle Bunker <kbunker@nwesd.org>
2016-10-18 13:08:31 -0700

    added setup script

---
+++ setup-ps-get.ps1
@@ -0,0 +1,18 @@
+﻿
+Find-module posh-git | install-module
+
+$MyPath = $env:Path + ";" + "${env:ProgramFiles}\git\cmd"
+[Environment]::SetEnvironmentVariable("Path", $($MyPath ), "Machine")
+
+$env:Path = $MyPath
+
+Import-Module posh-git
+
+$mod_version = Get-Module -name posh-git | select version
+$mod_version = $mod_version.Version.ToString()
+
+& "C:\Program Files\WindowsPowerShell\Modules\posh-git\$mod_version\Install.ps1"
+
+. $profile
+ #adding a comment
+
